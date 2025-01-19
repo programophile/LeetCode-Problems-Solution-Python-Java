e347836447f5ce8d3e2db03df994b07cc1c6e687
Sad Yeamin Sayem <98210531+programophile@users.noreply.github.com>
2025-01-20 04:23:41 +0600

    Python3 Solution
          Submission Statistics:
          Question #: 35
          Language: Python3
          Runtime: 2 ms
          Runtime percentile: 4.23
          Memory: 18.428 MB
          Memory percentile: 25.67

--- 35. Search Insert Position/Solution.py
+++ 35. Search Insert Position/Solution.py
@@ -1,4 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect.bisect_left(nums, target)
+        # Iterate through the list
+        for num in range(len(nums)):
+            # If target is found, return the index
+            if nums[num] == target:
+                return num
+            # If the current number is greater than the target, return the index
+            elif nums[num] > target:
+                return num
         
+        # If the target is greater than the last element, return the length of the list
+        return len(nums)
