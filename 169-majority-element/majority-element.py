cae24ddc80c5ddcec411f146922f949ca4d6062a
Sad Yeamin Sayem <98210531+programophile@users.noreply.github.com>
2025-10-25 07:55:56 +1200

    Time: 19 ms (16.88%) | Memory: 13.8 MB (32.27%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,17 @@
 class Solution(object):
     def majorityElement(self, nums):
-        nums.sort()
-        return nums[len(nums)//2]
+        dict={}
+        for i in nums:
+            if i not in dict.keys():
+                dict[i]=1
+            else:
+                dict[i]+=1
+        pair=[0,0]
+        for key,value in dict.items():
+            if value>pair[1]:
+                pair[0]=key
+                pair[1]=value
+        return pair[0]
+
+        
+        
