d97a75b78e611f57f30a001b69fed24422186dbe
Sad Yeamin Sayem <98210531+programophile@users.noreply.github.com>
2025-10-31 15:43:33 +1200

    Time: 347 ms (6.68%) | Memory: 45.8 MB (57.54%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max_index=0;
+        for (int i=0;i<nums.length;i++){
+            if (i>max_index){
+                return false;
+            }
+            int index=0;
+            index+=nums[i]+i;
+            if (index>max_index){
+                max_index=index;
+            }
+            System.out.println(max_index);
+            if (max_index>=nums.length-1){
+                return true;
+            }
+            
+        }
+        return false ;
+         
+
+        
+        
+    }
+}
