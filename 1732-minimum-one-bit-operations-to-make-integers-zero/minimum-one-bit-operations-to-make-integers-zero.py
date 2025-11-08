cfbaa99f208d62a0f17164909033c61a0dc1311e
Sad Yeamin Sayem <98210531+programophile@users.noreply.github.com>
2025-11-08 09:25:49 +0600

    Time: 0 ms (100.00%) | Memory: 12.3 MB (88.89%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minimumOneBitOperations(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        f = [0] * 31
+        f[0] = 1
+        for i in range(1, 31):
+            f[i] = 2 * f[i - 1] + 1
+
+        ans = 0
+        count = 0
+        for i in range(30, -1, -1):
+            if (1 << i) & n != 0:
+                count += 1
+                if count % 2 == 1:
+                    ans += f[i]
+                else:
+                    ans -= f[i]
+
+        return ans
+        
+        
