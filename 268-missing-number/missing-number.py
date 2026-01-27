8fee258fdef671553404ffe7c578968cb52f69ee
Sad Yeamin Sayem <98210531+programophile@users.noreply.github.com>
2026-01-27 14:19:00 +0600

    Time: 3 ms (59.06%) | Memory: 13.4 MB (65.14%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result=0
+        for i in range(len(nums)+1):
+            result+=i
+        return abs(result-sum(nums))
